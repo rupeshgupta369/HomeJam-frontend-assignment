0a89f73cbe8b30dac9d1e46dc3b97d7fd16f167e
rupeshgupta369 <rupeshgupta369@gmail.com>
2022-03-05 18:58:13 +0530

    minor style update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,49 +6,48 @@ import Calendar from '../../assets/icons/Calendar.png';
 import HomeStats from "../HomeStats/HomeStats"
 
 const statsData = [
-    {
-      value:60,
-      title:'label',
-      icon:Like,
-    },
-    {
-      value:0,
-      title:'label',
-      icon:Calendar,
-    },
-    {
-      value:0,
-      title:'label',
-      icon:Calendar,
-    },
-    {
-      value:0,
-      title:'label',
-      icon:Calendar,
-    },
-  ]
-  
-  const Home = () => {
-    return (
-      <div className='home__wrapper'>
-          <header>
-            <Header/>
-          </header>
-  
-          <div className="home__details">
-            <h1 className='home__title'>Cari Cari</h1>
-            <p className='home__tagline'>Live from their sofa to yours. Get closer to your favorite artists, and never miss out.</p>
-          </div>
-  
-          <div className="home__stats">
-            <HomeStats statsData={statsData[0]}/>
-            <HomeStats statsData={statsData[1]}/>
-            <HomeStats statsData={statsData[2]}/>
-            <HomeStats statsData={statsData[3]}/>
-          </div>
+  {
+    value: 60,
+    title: 'label',
+    icon: Like,
+  },
+  {
+    value: 0,
+    title: 'label',
+    icon: Calendar,
+  },
+  {
+    value: 0,
+    title: 'label',
+    icon: Calendar,
+  },
+  {
+    value: 0,
+    title: 'label',
+    icon: Calendar,
+  },
+]
+
+const Home = () => {
+  return (
+    <div className='home__wrapper'>
+      <header>
+        <Header />
+      </header>
+
+      <div className="home__details">
+        <h1 className='home__title'>The Batman</h1>
+        <p className='home__tagline'>When the Riddler, a sadistic serial killer, begins murdering key political figures in Gotham, Batman is forced to investigate the city's hidden corruption and question his family's involvement.</p>
       </div>
-    );
-  };
-  
-  export default Home;
-  
+
+      <div className="home__stats">
+        <HomeStats statsData={statsData[0]} />
+        <HomeStats statsData={statsData[1]} />
+        <HomeStats statsData={statsData[2]} />
+        <HomeStats statsData={statsData[3]} />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
